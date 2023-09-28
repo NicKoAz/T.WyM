469c32c78d1bc373e33359e2d7bd339f0319ff55
Nicolás Carreño <nicobk.carreno@gmail.com>
2023-09-28 10:02:00 -0300

    actualizacion

--- GUIAS/JS/ContadorPalabras.js
+++ GUIAS/JS/ContadorPalabras.js
@@ -2,14 +2,13 @@ function contarPalabras() {
     var oracion = prompt("Por favor, ingrese una oración:");
 
     if (oracion !== null && oracion.trim() !== "") {
-        // Verificar si la oración contiene números
         if (!/\d/.test(oracion)) {
             var palabras = oracion.split(/\s+/);
 
             alert("Oración ingresada: " + oracion + "\nNúmero de palabras: " + palabras.length);
         } else {
             alert("Incorrecto, solo ingrese palabras.");
-            contarPalabras(); // Llama a la función nuevamente para intentar con una nueva entrada.
+            contarPalabras(); 
         }
     } else {
         alert("No ingresó una oración válida.");
